92dcc2b393495e3b2c2fdbd9807c62ba858c0c44
Adrien Viviani <adrien.viv@gmail.com>
2023-07-07 18:17:20 +0200

    add pluck into console

--- .pythonrc.py
+++ .pythonrc.py
@@ -12,3 +12,7 @@ print("Welcome to Python!")
 
 maison = Property.objects.get(id=1)
 qq = exit
+
+# pluck(Property.objects.all(), 'title')
+def pluck(objects, attribute):
+    return list(map(lambda obj: getattr(obj, attribute), objects))
