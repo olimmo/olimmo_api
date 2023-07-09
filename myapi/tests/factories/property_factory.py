51ef4ccf017a5a077e5cc7d47bab48888a7aeabf
Adrien Viviani <adrien.viv@gmail.com>
2023-07-09 19:07:07 +0200

    improve tests

--- myapi/tests/factories/property_factory.py
+++ myapi/tests/factories/property_factory.py
@@ -1,14 +1,17 @@
 import factory
 from myapi.models import Property
 
+from .seller_factory import SellerFactory
+
 
 class PropertyFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Property
 
-    address = factory.Faker("street_address")
+    address = factory.Faker("address")
     city = factory.Faker("city")
-    postal_code = factory.Faker("postcode")
     country = factory.Faker("country")
+    postal_code = factory.Faker("zipcode")
     surface = factory.Faker("random_int", min=0, max=100000)
-    title = factory.Faker("sentence", nb_words=4)
+    title = factory.Sequence(lambda n: f"Property {n}")
+    seller = factory.SubFactory(SellerFactory)
