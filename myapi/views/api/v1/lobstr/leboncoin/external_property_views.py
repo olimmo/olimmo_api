b22430914f6d54c6f06f0c4e45a9bb8f7aaf149a
Adrien Viviani <adrien.viv@gmail.com>
2023-08-22 17:15:45 +0200

    remove seller and property

--- myapi/views/api/v1/lobstr/leboncoin/external_property_views.py
+++ myapi/views/api/v1/lobstr/leboncoin/external_property_views.py
@@ -1,5 +1,4 @@
-from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 
@@ -8,11 +7,12 @@ from myapi.tasks.lobstr.leboncoin.external_property_create_task import (
 )
 
 
-class ExternalPropertyListView(APIView):
-    def post(self, request, format=None):
+class LobstrLeboncoinExternalPropertyViewSet(viewsets.ModelViewSet):
+    def create(self, request, *args, **kwargs):
         run_id = request.data.get("id")
         if not run_id:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
         create_external_property_task.delay(run_id)
 
         return Response({"status": "Job started"}, status=status.HTTP_202_ACCEPTED)
