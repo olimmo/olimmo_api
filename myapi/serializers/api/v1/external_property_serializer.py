5b1ee984c862393106d457c8b65c76f0c69f64c0
Adrien Viviani <adrien.viv@gmail.com>
2023-08-24 20:06:04 +0200

    add audits to serializers

--- myapi/serializers/api/v1/external_property_serializer.py
+++ myapi/serializers/api/v1/external_property_serializer.py
@@ -1,12 +1,15 @@
+from rest_framework import serializers
+
 from myapi.models import ExternalProperty
 from myapi.serializers.api.v1.base_serializer import BaseSerializer
 from myapi.serializers.api.v1.photo_serializer import PhotoSerializer
 
 
 class ExternalPropertySerializer(BaseSerializer):
+    audits = serializers.SerializerMethodField()
     photos = PhotoSerializer(many=True)
 
     class Meta:
         model = ExternalProperty
         fields = "__all__"
-        include_fields = ["photos"]
+        include_fields = ["audits", "photos"]
